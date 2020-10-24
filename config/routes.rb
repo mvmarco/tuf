6c4530b0a61a3a45b08e9140cc4aaf08def6dde2
marco corapi <marco.corapi@outlook.it>
2020-10-24 18:25:20 +0200

    Draft form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'new', to: 'pages#new'
+  get 'create', to: 'pages#create'
   get "/donate", to: "pages#donate"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
