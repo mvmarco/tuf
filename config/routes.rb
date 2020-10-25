f19be660dbd8c204e26eb917fc1a2abcb5b9b586
marco corapi <marco.corapi@outlook.it>
2020-10-25 15:40:16 +0100

    small changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'new', to: 'pages#new'
   get 'create', to: 'pages#create'
   get "/donate", to: "pages#donate"
+
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
