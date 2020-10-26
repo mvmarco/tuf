855d977374804ac33d4a45d4a0834d2efaa9df9b
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-10-26 16:11:57 +0000

    moved donation link to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   post "send_contact", to: "pages#send_contact"
-  get "/donate", to: "pages#donate"
 
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
